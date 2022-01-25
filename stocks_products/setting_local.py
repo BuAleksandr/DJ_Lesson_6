79388c9addf9d2e2e45ac089ec18f665938ad8e9
BuAleksandr <87057144+BuAleksandr@users.noreply.github.com>
2022-01-25 22:17:48 +1100

    Fix serializers and views

---
+++ stocks_products/setting_local.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'orm',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'USER': 'user_orm',
+        'PASSWORD': 'gfhjkm'
+    }
+}
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-nw^y+m^wmxza1asgk+)!ua2qx9)g+#v=6%76-9i8i(6eqiw94j'
